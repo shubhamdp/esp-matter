1091fe0d38f9bf9d52ef17a3716c19ff4a507891
Shubham Patil <shubham.patil@espressif.com>
2024-12-17 13:33:33 +0530

    add blecent_scan and bleprph_advertise

--- examples/light/main/app_main.cpp
+++ examples/light/main/app_main.cpp
@@ -17,6 +17,8 @@
 #include <common_macros.h>
 #include <app_priv.h>
 #include <app_reset.h>
+#include <sys/types.h>
+#include "freertos/idf_additions.h"
 #if CHIP_DEVICE_CONFIG_ENABLE_THREAD
 #include <platform/ESP32/OpenthreadLauncher.h>
 #endif
@@ -32,6 +34,7 @@ using namespace esp_matter::attribute;
 using namespace esp_matter::endpoint;
 using namespace chip::app::Clusters;
 
+static bool startAdv = false;
 constexpr auto k_timeout_seconds = 300;
 
 #if CONFIG_ENABLE_ENCRYPTED_OTA
@@ -51,6 +54,7 @@ static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
 
     case chip::DeviceLayer::DeviceEventType::kCommissioningComplete:
         ESP_LOGI(TAG, "Commissioning complete");
+        startAdv = true;
         break;
 
     case chip::DeviceLayer::DeviceEventType::kFailSafeTimerExpired:
@@ -143,6 +147,518 @@ static esp_err_t app_attribute_update_cb(attribute::callback_type_t type, uint16
     return err;
 }
 
+#define GATT_SVR_SVC_ALERT_UUID               0x1811
+
+static void bleprph_advertise();
+
+static int bleprph_gap_event(struct ble_gap_event *event, void *arg)
+{
+    struct ble_gap_conn_desc desc;
+    int rc;
+
+    switch (event->type) {
+    case BLE_GAP_EVENT_CONNECT:
+        /* A new connection was established or a connection attempt failed. */
+        ESP_LOGE(TAG, "connection %s; status=%d ",
+                    event->connect.status == 0 ? "established" : "failed",
+                    event->connect.status);
+        if (event->connect.status == 0) {
+            rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
+            if (rc != 0) {
+                ESP_LOGE(TAG, "ble_gap_conn_find failed");
+                break;
+            }
+            /*bleprph_print_conn_desc(&desc);*/
+        }
+        ESP_LOGE(TAG, "\n");
+
+        if (event->connect.status != 0) {
+            bleprph_advertise();
+        }
+
+        return 0;
+
+    case BLE_GAP_EVENT_DISCONNECT:
+        ESP_LOGE(TAG, "disconnect; reason=%d ", event->disconnect.reason);
+        /*bleprph_print_conn_desc(&event->disconnect.conn);*/
+        ESP_LOGE(TAG, "\n");
+
+        bleprph_advertise();
+        return 0;
+
+    case BLE_GAP_EVENT_CONN_UPDATE:
+        /* The central has updated the connection parameters. */
+        ESP_LOGE(TAG, "connection updated; status=%d ",
+                    event->conn_update.status);
+        rc = ble_gap_conn_find(event->conn_update.conn_handle, &desc);
+            if (rc != 0) {
+                ESP_LOGE(TAG, "ble_gap_conn_find failed");
+                break;
+            }
+        /*bleprph_print_conn_desc(&desc);*/
+        ESP_LOGE(TAG, "\n");
+        return 0;
+
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        ESP_LOGE(TAG, "advertise complete; reason=%d",
+                    event->adv_complete.reason);
+        return 0;
+
+    case BLE_GAP_EVENT_ENC_CHANGE:
+        /* Encryption has been enabled or disabled for this connection. */
+        ESP_LOGE(TAG, "encryption change event; status=%d ",
+                    event->enc_change.status);
+        rc = ble_gap_conn_find(event->enc_change.conn_handle, &desc);
+            if (rc != 0) {
+                ESP_LOGE(TAG, "ble_gap_conn_find failed");
+                break;
+            }
+        /*bleprph_print_conn_desc(&desc);*/
+        ESP_LOGE(TAG, "\n");
+        return 0;
+
+    case BLE_GAP_EVENT_SUBSCRIBE:
+        ESP_LOGE(TAG, "subscribe event; conn_handle=%d attr_handle=%d "
+                    "reason=%d prevn=%d curn=%d previ=%d curi=%d\n",
+                    event->subscribe.conn_handle,
+                    event->subscribe.attr_handle,
+                    event->subscribe.reason,
+                    event->subscribe.prev_notify,
+                    event->subscribe.cur_notify,
+                    event->subscribe.prev_indicate,
+                    event->subscribe.cur_indicate);
+        return 0;
+
+    case BLE_GAP_EVENT_MTU:
+        ESP_LOGE(TAG, "mtu update event; conn_handle=%d cid=%d mtu=%d\n",
+                    event->mtu.conn_handle,
+                    event->mtu.channel_id,
+                    event->mtu.value);
+        return 0;
+
+    case BLE_GAP_EVENT_REPEAT_PAIRING:
+        /* We already have a bond with the peer, but it is attempting to
+         * establish a new secure link.  This app sacrifices security for
+         * convenience: just throw away the old bond and accept the new link.
+         */
+
+        /* Delete the old bond. */
+        rc = ble_gap_conn_find(event->repeat_pairing.conn_handle, &desc);
+            if (rc != 0) {
+                ESP_LOGE(TAG, "ble_gap_conn_find failed");
+                break;
+            }
+        ble_store_util_delete_peer(&desc.peer_id_addr);
+
+        /* Return BLE_GAP_REPEAT_PAIRING_RETRY to indicate that the host should
+         * continue with the pairing operation.
+         */
+        return BLE_GAP_REPEAT_PAIRING_RETRY;
+
+    default:
+        break;
+    }
+    return 0;
+}
+
+static void bleprph_advertise()
+{
+    ble_gatts_reset();
+
+    ESP_LOGE(TAG, "Starting advertising");
+
+    struct ble_gap_adv_params adv_params;
+    struct ble_hs_adv_fields fields;
+    const char *name;
+    int rc;
+
+    /**
+     *  Set the advertisement data included in our advertisements:
+     *     o Flags (indicates advertisement type and other general info).
+     *     o Advertising tx power.
+     *     o Device name.
+     *     o 16-bit service UUIDs (alert notifications).
+     */
+    memset(&fields, 0, sizeof fields);
+
+    /* Advertise two flags:
+     *     o Discoverability in forthcoming advertisement (general)
+     *     o BLE-only (BR/EDR unsupported).
+     */
+    fields.flags = BLE_HS_ADV_F_DISC_GEN |
+                   BLE_HS_ADV_F_BREDR_UNSUP;
+
+    /* Indicate that the TX power level field should be included; have the
+     * stack fill this value automatically.  This is done by assigning the
+     * special value BLE_HS_ADV_TX_PWR_LVL_AUTO.
+     */
+    fields.tx_pwr_lvl_is_present = 1;
+    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
+
+    /*name = ble_svc_gap_device_name();*/
+    name = "lateral-adv";
+    fields.name = (uint8_t *)name;
+    fields.name_len = strlen(name);
+    fields.name_is_complete = 1;
+
+    static ble_uuid16_t alertUuids[] = {
+        BLE_UUID16_INIT(GATT_SVR_SVC_ALERT_UUID)
+    };
+
+    fields.uuids16 = alertUuids;
+    fields.num_uuids16 = 1;
+    fields.uuids16_is_complete = 1;
+
+    rc = ble_gap_adv_set_fields(&fields);
+    if (rc != 0) {
+        ESP_LOGE(TAG, "error setting advertisement data; rc=%d\n", rc);
+        return;
+    }
+
+    uint8_t own_addr_type;
+    rc = ble_hs_id_infer_auto(0, &own_addr_type);
+    if (rc != 0) {
+        ESP_LOGE(TAG, "error determining address type; rc=%d\n", rc);
+        return;
+    }
+
+    /* Begin advertising. */
+    memset(&adv_params, 0, sizeof adv_params);
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    rc = ble_gap_adv_start(own_addr_type, NULL, BLE_HS_FOREVER,
+                           &adv_params, bleprph_gap_event, NULL);
+    if (rc != 0) {
+        ESP_LOGE(TAG, "error enabling advertisement; rc=%d\n", rc);
+        return;
+    }
+}
+
+void print_bytes(const uint8_t *bytes, int len)
+{
+    int i;
+
+    for (i = 0; i < len; i++) {
+        ESP_LOGE(TAG, "%s0x%02x", i != 0 ? ":" : "", bytes[i]);
+    }
+}
+
+void print_uuid(const ble_uuid_t *uuid)
+{
+    char buf[BLE_UUID_STR_LEN];
+
+    ESP_LOGE(TAG, "%s", ble_uuid_to_str(uuid, buf));
+}
+
+char *addr_str(const void *addr)
+{
+    static char buf[6 * 2 + 5 + 1];
+    const uint8_t *u8p;
+
+    u8p = (const uint8_t *)addr;
+    sprintf(buf, "%02x:%02x:%02x:%02x:%02x:%02x",
+            u8p[5], u8p[4], u8p[3], u8p[2], u8p[1], u8p[0]);
+
+    return buf;
+}
+
+void print_adv_fields(const struct ble_hs_adv_fields *fields)
+{
+    char s[BLE_HS_ADV_MAX_SZ];
+    const uint8_t *u8p;
+    int i;
+
+    if (fields->flags != 0) {
+        ESP_LOGE(TAG, "    flags=0x%02x\n", fields->flags);
+    }
+
+    if (fields->uuids16 != NULL) {
+        ESP_LOGE(TAG, "    uuids16(%scomplete)=",
+                    fields->uuids16_is_complete ? "" : "in");
+        for (i = 0; i < fields->num_uuids16; i++) {
+            print_uuid(&fields->uuids16[i].u);
+            ESP_LOGE(TAG, " ");
+        }
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->uuids32 != NULL) {
+        ESP_LOGE(TAG, "    uuids32(%scomplete)=",
+                    fields->uuids32_is_complete ? "" : "in");
+        for (i = 0; i < fields->num_uuids32; i++) {
+            print_uuid(&fields->uuids32[i].u);
+            ESP_LOGE(TAG, " ");
+        }
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->uuids128 != NULL) {
+        ESP_LOGE(TAG, "    uuids128(%scomplete)=",
+                    fields->uuids128_is_complete ? "" : "in");
+        for (i = 0; i < fields->num_uuids128; i++) {
+            print_uuid(&fields->uuids128[i].u);
+            ESP_LOGE(TAG, " ");
+        }
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->name != NULL) {
+        assert(fields->name_len < sizeof s - 1);
+        memcpy(s, fields->name, fields->name_len);
+        s[fields->name_len] = '\0';
+        ESP_LOGE(TAG, "    name(%scomplete)=%s\n",
+                    fields->name_is_complete ? "" : "in", s);
+    }
+
+    if (fields->tx_pwr_lvl_is_present) {
+        ESP_LOGE(TAG, "    tx_pwr_lvl=%d\n", fields->tx_pwr_lvl);
+    }
+
+    if (fields->slave_itvl_range != NULL) {
+        ESP_LOGE(TAG, "    slave_itvl_range=");
+        print_bytes(fields->slave_itvl_range, BLE_HS_ADV_SLAVE_ITVL_RANGE_LEN);
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->svc_data_uuid16 != NULL) {
+        ESP_LOGE(TAG, "    svc_data_uuid16=");
+        print_bytes(fields->svc_data_uuid16, fields->svc_data_uuid16_len);
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->public_tgt_addr != NULL) {
+        ESP_LOGE(TAG, "    public_tgt_addr=");
+        u8p = fields->public_tgt_addr;
+        for (i = 0; i < fields->num_public_tgt_addrs; i++) {
+            ESP_LOGE(TAG, "public_tgt_addr=%s ", addr_str(u8p));
+            u8p += BLE_HS_ADV_PUBLIC_TGT_ADDR_ENTRY_LEN;
+        }
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->appearance_is_present) {
+        ESP_LOGE(TAG, "    appearance=0x%04x\n", fields->appearance);
+    }
+
+    if (fields->adv_itvl_is_present) {
+        ESP_LOGE(TAG, "    adv_itvl=0x%04x\n", fields->adv_itvl);
+    }
+
+    if (fields->svc_data_uuid32 != NULL) {
+        ESP_LOGE(TAG, "    svc_data_uuid32=");
+        print_bytes(fields->svc_data_uuid32, fields->svc_data_uuid32_len);
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->svc_data_uuid128 != NULL) {
+        ESP_LOGE(TAG, "    svc_data_uuid128=");
+        print_bytes(fields->svc_data_uuid128, fields->svc_data_uuid128_len);
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->uri != NULL) {
+        ESP_LOGE(TAG, "    uri=");
+        print_bytes(fields->uri, fields->uri_len);
+        ESP_LOGE(TAG, "\n");
+    }
+
+    if (fields->mfg_data != NULL) {
+        ESP_LOGE(TAG, "    mfg_data=");
+        print_bytes(fields->mfg_data, fields->mfg_data_len);
+        ESP_LOGE(TAG, "\n");
+    }
+}
+
+static void blecent_scan();
+
+static int blecent_gap_event(struct ble_gap_event *event, void *arg)
+{
+    printf("ble_gap_event\n\n");
+    struct ble_gap_conn_desc desc;
+    struct ble_hs_adv_fields fields;
+    int rc;
+
+    switch (event->type) {
+    case BLE_GAP_EVENT_DISC:
+        rc = ble_hs_adv_parse_fields(&fields, event->disc.data,
+                                     event->disc.length_data);
+        if (rc != 0) {
+            return 0;
+        }
+
+        /* An advertisement report was received during GAP discovery. */
+        print_adv_fields(&fields);
+
+        /* Try to connect to the advertiser if it looks interesting. */
+        /*blecent_connect_if_interesting(&event->disc);*/
+
+        rc = ble_gap_disc_cancel();
+        if (rc != 0) {
+            ESP_LOGE(TAG, "Failed to cancel scan; rc=%d\n", rc);
+            return 0;
+        }
+
+        uint8_t own_addr_type;
+        rc = ble_hs_id_infer_auto(0, &own_addr_type);
+        if (rc != 0) {
+            ESP_LOGE(TAG, "error determining address type; rc=%d\n", rc);
+            return 0;
+        }
+
+        ble_addr_t *addr;
+        addr = &(event->disc.addr);
+
+        rc = ble_gap_connect(own_addr_type, addr, 30000, NULL,
+                             blecent_gap_event, NULL);
+        if (rc != 0) {
+            ESP_LOGE(TAG, "Error: Failed to connect to device; addr_type=%d "
+                        "addr=%s; rc=%d\n",
+                        addr->type, addr_str(addr->val), rc);
+        }
+
+        return 0;
+
+    case BLE_GAP_EVENT_LINK_ESTAB:
+        /* A new connection was established or a connection attempt failed. */
+        if (event->connect.status == 0) {
+            /* Connection successfully established. */
+            ESP_LOGE(TAG, "Connection established ");
+
+            rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
+            assert(rc == 0);
+            /*print_conn_desc(&desc);*/
+            ESP_LOGE(TAG, "\n");
+
+            /* Remember peer. */
+            /*rc = peer_add(event->connect.conn_handle);*/
+            /*if (rc != 0) {*/
+            /*    ESP_LOGE(TAG, "Failed to add peer; rc=%d\n", rc);*/
+            /*    return 0;*/
+            /*}*/
+
+            /* Perform service discovery */
+            /*rc = peer_disc_all(event->connect.conn_handle,*/
+            /*            blecent_on_disc_complete, NULL);*/
+            /*if(rc != 0) {*/
+            /*    ESP_LOGE(TAG, "Failed to discover services; rc=%d\n", rc);*/
+            /*    return 0;*/
+            /*}*/
+        } else {
+            /* Connection attempt failed; resume scanning. */
+            /*ESP_LOGE(TAG, "Error: Connection failed; status=%d\n",*/
+            /*            event->connect.status);*/
+            blecent_scan();
+        }
+
+        return 0;
+
+    case BLE_GAP_EVENT_DISCONNECT:
+        /* Connection terminated. */
+        ESP_LOGE(TAG, "disconnect; reason=%d ", event->disconnect.reason);
+        /*print_conn_desc(&event->disconnect.conn);*/
+        ESP_LOGE(TAG, "\n");
+
+        /* Forget about peer. */
+        /*peer_delete(event->disconnect.conn.conn_handle);*/
+
+        /* Resume scanning. */
+        blecent_scan();
+        return 0;
+
+    case BLE_GAP_EVENT_DISC_COMPLETE:
+        ESP_LOGE(TAG, "discovery complete; reason=%d\n",
+                    event->disc_complete.reason);
+        return 0;
+
+    case BLE_GAP_EVENT_ENC_CHANGE:
+        /* Encryption has been enabled or disabled for this connection. */
+        ESP_LOGE(TAG, "encryption change event; status=%d ",
+                    event->enc_change.status);
+        rc = ble_gap_conn_find(event->enc_change.conn_handle, &desc);
+        assert(rc == 0);
+        /*print_conn_desc(&desc);*/
+        return 0;
+
+    case BLE_GAP_EVENT_NOTIFY_RX:
+        /* Peer sent us a notification or indication. */
+        ESP_LOGE(TAG, "received %s; conn_handle=%d attr_handle=%d "
+                    "attr_len=%d\n",
+                    event->notify_rx.indication ?
+                    "indication" :
+                    "notification",
+                    event->notify_rx.conn_handle,
+                    event->notify_rx.attr_handle,
+                    OS_MBUF_PKTLEN(event->notify_rx.om));
+
+        /* Attribute data is contained in event->notify_rx.om. Use
+         * `os_mbuf_copydata` to copy the data received in notification mbuf */
+        return 0;
+
+    case BLE_GAP_EVENT_MTU:
+        ESP_LOGE(TAG, "mtu update event; conn_handle=%d cid=%d mtu=%d\n",
+                    event->mtu.conn_handle,
+                    event->mtu.channel_id,
+                    event->mtu.value);
+        return 0;
+
+    case BLE_GAP_EVENT_REPEAT_PAIRING:
+        /* We already have a bond with the peer, but it is attempting to
+         * establish a new secure link.  This app sacrifices security for
+         * convenience: just throw away the old bond and accept the new link.
+         */
+
+        /* Delete the old bond. */
+        rc = ble_gap_conn_find(event->repeat_pairing.conn_handle, &desc);
+        assert(rc == 0);
+        ble_store_util_delete_peer(&desc.peer_id_addr);
+
+        /* Return BLE_GAP_REPEAT_PAIRING_RETRY to indicate that the host should
+         * continue with the pairing operation.
+         */
+        return BLE_GAP_REPEAT_PAIRING_RETRY;
+    default:
+        return 0;
+    }
+}
+
+static void blecent_scan(void)
+{
+    uint8_t own_addr_type;
+    struct ble_gap_disc_params disc_params;
+    int rc;
+
+    /* Figure out address to use while advertising (no privacy for now) */
+    rc = ble_hs_id_infer_auto(0, &own_addr_type);
+    if (rc != 0) {
+        ESP_LOGE(TAG, "error determining address type; rc=%d\n", rc);
+        return;
+    }
+
+    /* Tell the controller to filter duplicates; we don't want to process
+     * repeated advertisements from the same device.
+     */
+    disc_params.filter_duplicates = 1;
+
+    /**
+     * Perform a passive scan.  I.e., don't send follow-up scan requests to
+     * each advertiser.
+     */
+    disc_params.passive = 1;
+
+    /* Use defaults for the rest of the parameters. */
+    disc_params.itvl = 0;
+    disc_params.window = 0;
+    disc_params.filter_policy = 0;
+    disc_params.limited = 0;
+
+    rc = ble_gap_disc(own_addr_type, BLE_HS_FOREVER, &disc_params,
+                      blecent_gap_event, NULL);
+    if (rc != 0) {
+        ESP_LOGE(TAG, "Error initiating GAP discovery procedure; rc=%d\n",
+                    rc);
+    }
+}
+
 extern "C" void app_main()
 {
     esp_err_t err = ESP_OK;
@@ -230,4 +746,18 @@ extern "C" void app_main()
 #endif
     esp_matter::console::init();
 #endif
+
+    if (chip::Server::GetInstance().GetFabricTable().FabricCount() > 0)
+    {
+        startAdv = true;
+    }
+
+    while (!startAdv) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+    /*bleprph_advertise();*/
+    blecent_scan();
 }
